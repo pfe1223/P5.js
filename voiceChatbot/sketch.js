72ab7d850e97e629867f201c5149f108c8b59126
pfe1223 <patrickester@APP-1554.local>
2018-04-11 13:23:56 -0700

    Voice Chatbot
    Initial dump for the voice chatbot explore class project. This is a
    copy from Dan Shiffman’s work.

---
+++ voiceChatbot/sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  noCanvas();
+  let speech = new p5.Speech();
+  let speechRec = new p5.SpeechRec('en-US', gotSpeech);
+  let continuous = true;
+  let interim = false;
+  speechRec.start(continuous, interim);
+
+  let bot = new RiveScript();
+  bot.loadFile("brain.txt", brainReady, brainError);
+
+  function brainReady() {
+    console.log('Chatbot ready!');
+    bot.sortReplies();
+  }
+
+  function brainError() {
+    console.log('Chatbot error!')
+  }
+
+  function gotSpeech() {
+    if (speechRec.resultValue) {
+      let input = speechRec.resultString;
+      //user_input.value(input);
+      let reply = bot.reply("local-user", input);
+      speech.speak(reply);
+      //output.html(reply);
+    }
+  }
+}
